f6a9d6919fba80df939de1a2dcc57ace59676915
meriam <hamdaoui.meriam85@gmail.com>
2022-07-17 17:59:29 +0200

    .env

--- client/src/api/fetchs.js
+++ client/src/api/fetchs.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const fetchAds = async () => {
-  const { data } = await axios.get("http://localhost:5000/farah");
+  const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/farah`);
   return data;
 };
